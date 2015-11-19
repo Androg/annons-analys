05828c8191c7b4d0ba3b51fd7c1e7f655b0a3ae0
Hussein <hussein.albreihi@hotmail.com>
2015-11-19 12:51:16 +0100

    added GET method to get title from jobad

---
+++ target/classes/se.eh/UserService.java
@@ -0,0 +1,13 @@
+package se.eh;
+
+import se.eh.User;
+
+public class UserService {
+
+    public User getDefaultUser() {
+        User user = new User();
+        user.setFirstName("JonFromREST");
+        user.setLastName("DoeFromREST");
+        return user;
+    }
+}
