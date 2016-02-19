f9a3bfefa32ea244b0d673c1351a9fbe82afa726
Hussein <hussein.albreihi@hotmail.com>
2016-02-19 20:23:12 +0100

    added sidemenu

--- src/main/webapp/scripts/controllers/main.js
+++ src/main/webapp/scripts/controllers/main.js
@@ -4,14 +4,31 @@ angular.module('annons-analys').controller(
 		'MainCtrl',
 		[
                 '$scope',
+                '$rootScope',
 				'$http',
 				'$location',
 				'localStorageService',
 				'myFactory',
-				function($scope, $http, $location, localStorageService,
+				function($scope, $rootScope, $http, $location, localStorageService,
 						myFactory) {
 
 					$scope.url = {};
+
+                    $scope.keywords = ["HEJ", "bajs", "test"];
+
+                    $scope.state = false;
+
+                    $scope.toggleState = function() {
+                        $scope.state = !$scope.state;
+                        if($scope.state === true) {
+                            console.log($scope.state);
+                            document.getElementById("sidebar").classList.add("show");
+                        } else {
+                            console.log($scope.state);
+                            document.getElementById("sidebar").classList.remove("show");
+                        }
+
+                    };
 
                         $scope.validateURL = function(textval) {
                         var urlregex = /^(http|https):\/\/(([a-zA-Z0-9$\-_.+!*'(),;:&=]|%[0-9a-fA-F]{2})+@)?(((25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])){3})|localhost|([a-zA-Z0-9\-\u00C0-\u017F]+\.)+([a-zA-Z]{2,}))(:[0-9]+)?(\/(([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*(\/([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*)*)?(\?([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?(\#([a-zA-Z0-9$\-_.+!*'(),;:@&=\/?]|%[0-9a-fA-F]{2})*)?)?$/;
@@ -29,6 +46,8 @@ angular.module('annons-analys').controller(
 
                     $scope.start = function () {
                     	$location.path('main');
-                    }
+                    };
+
+
 
 				} ]);
